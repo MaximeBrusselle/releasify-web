c3375c2418ec926fa7795a11496b16c1d7d2eb15
Maxime Brusselle <brussma@cronos.be>
2024-04-21 16:20:19 +0200

    add dependencies

--- src/pages/artists/ArtistsPage.tsx
+++ src/pages/artists/ArtistsPage.tsx
@@ -1,8 +1,11 @@
+import { Badge } from "@/components/ui/badge"
+
 const ArtistsPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Welcome to the ArtistsPage
+        
       </h1>
       {/* Content */}
     </div>
