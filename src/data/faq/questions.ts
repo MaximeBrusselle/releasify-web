f177399a004becdfa177cffde0c0cb2a8477c61a
Maxime Brusselle <brussma@cronos.be>
2024-05-22 16:34:53 +0200

    add answers to faq

--- src/data/faq/questions.ts
+++ src/data/faq/questions.ts
@@ -5,73 +5,73 @@ const questions: Questions = {
         {
             id: "Artist1",
             question: "How do I make an artist profile?",
-            answer: "TODO",
+            answer: "To make an artist profile u need to register as an artist. You can do this by going to the registration page and selecting the register as an artist option. After that fill out your details and the account will be created for you.",
         },
         {
             id: "Artist2",
             question: "How do I announce a new release?",
-            answer: "TODO",
+            answer: "You can announce a new release by going to the dashboard and selecting the releases tab. Here you can add a new release by clicking the add release button at the top of the page. Fill out the details and the release will be added to the system.",
         },
         {
             id: "Artist3",
             question: "How do I edit my profile?",
-            answer: "TODO",
+            answer: "You can edit your profile in the dashboard by going to the user tab. Once you click on the edit button a form should appear. Here you can edit your details and save them to the system.",
         }
     ],
     "Label": [
         {
             id: "Label1",
             question: "How do I make a label profile?",
-            answer: "TODO",
+            answer: "To make an label profile u need to register as a label. You can do this by going to the registration page and selecting the register as a label option. After that fill out your details and the account will be created for you.",
         },
         {
             id: "Label2",
             question: "How do I add an artist to my label?",
-            answer: "TODO",
+            answer: "You can add artists to your label by going to the dashboard and selecting the artists tab. Here you can add new artists by clicking the add artists button at the top of the page. Fill out the details and the artists will be added to your label",
         },
         {
             id: "Label3",
             question: "How do I edit my label profile?",
-            answer: "TODO",
+            answer: "You can edit your profile in the dashboard by going to the user tab. Once you click on the edit button a form should appear. Here you can edit your details and save them to the system.",
         },
         {
             id: "Label4",
             question: "How do I announce a release for one of my artists?",
-            answer: "TODO",
+            answer: "You can announce a new release by going to the dashboard and selecting the releases tab. Here you can add a new release by clicking the add release button at the top of the page. Fill out the details and the release will be added to the system.",
         }
     ],
     "Fan": [
         {
             id: "Fan1",
             question: "How do I make a profile?",
-            answer: "TODO",
+            answer: "To make a profile you need to register as a fan. You can do this by going to the registration page and selecting the register as a fan option. After that fill out your details and the account will be created for you.",
         },
         {
             id: "Fan2",
             question: "How do I follow an artist?",
-            answer: "TODO",
+            answer: "You can follow an artist by going to their profile and clicking the follow button. This will add the artist to your following list and you will be able to see their releases in your feed.",
         },
         {
             id: "Fan3",
             question: "How do I follow a label?",
-            answer: "TODO",
+            answer: "You can follow a label by going to their profile and clicking the follow button. This will add the label to your following list and you will be able to see their releases in your feed.",
         },
         {
             id: "Fan4",
             question: "How do I edit my profile?",
-            answer: "TODO",
+            answer: "You can edit your profile in the dashboard by going to the user tab. Once you click on the edit button a form should appear. Here you can edit your details and save them to the system.",
         },
     ],
     "Other": [
         {
             id: "Other1",
             question: "How do I report a bug?",
-            answer: "TODO",
+            answer: "This option is not implemented yet but will be soon.",
         },
         {
             id: "Other2",
             question: "How do I contact support?",
-            answer: "TODO",
+            answer: "This option is not implemented yet but will be soon.",
         },
     ]
 }
