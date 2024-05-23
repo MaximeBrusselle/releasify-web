ed21f70f7b05a7d7a73800105a72aa429deb3a45
Maxime Brusselle <brussma@cronos.be>
2024-05-23 02:36:38 +0200

    change env

--- src/data/api/other/imgbbUpload.ts
+++ src/data/api/other/imgbbUpload.ts
@@ -5,18 +5,18 @@ import axios from "axios";
 // dotenv.config();
 
 const imgbbUpload = async (image: File) => {
-    if (!process.env.IMGBB_API_KEY) {
+    if (!import.meta.env.VITE_IMGBB_API_KEY) {
         throw new Error("IMGBB_API_KEY is not defined");
     };
     if (!image) {
         throw new Error("Image is required");
     };
-    if(process.env.ENVIRONMENT === "development") {
+    if(import.meta.env.VITE_ENVIRONMENT === "development") {
         throw new Error("Image upload is disabled in development mode");
     }
 	const formData = new FormData();
 	formData.append("image", image);
-	formData.append("key", process.env.IMGBB_API_KEY!);
+	formData.append("key", import.meta.env.VITE_IMGBB_API_KEY!);
 
 	const response = await axios.post("https://api.imgbb.com/1/upload", formData, {
 		headers: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   return {
-    define: {
-      'process.env.IMGBB_API_KEY': JSON.stringify(env.IMGBB_API_KEY),
-      'process.env.ENVIRONMENT': JSON.stringify(env.ENVIRONMENT)
-    },
     plugins: [react()],
     resolve: {
       alias: {
